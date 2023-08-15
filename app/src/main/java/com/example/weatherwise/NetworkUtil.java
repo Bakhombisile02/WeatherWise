b7783f76dda415ac4035fd4f5aa57eecbab4e764
Bakhombisile02 <127313114+Bakhombisile02@users.noreply.github.com>
2023-08-15 07:55:34 +0200

    NetworkUtil with URL method

--- app/src/main/java/com/example/weatherwise/NetworkUtil.java
+++ app/src/main/java/com/example/weatherwise/NetworkUtil.java
@@ -1,7 +1,14 @@
 package com.example.weatherwise;
 
-public class NetworkUtil {
+import android.net.Uri;
+import android.util.Log;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class NetworkUtil
+{
+    public static final String TAG = "ourURL";
     // This constant holds the base URL for the AccuWeather API endpoint that provides 5-day forecasts.
     public static final String BASE_URL = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/306633";
 
@@ -17,4 +24,29 @@ public class NetworkUtil {
 
     // This constant represents the name of the query parameter used to send your API key with each request.
     public static final String API_PARAM = "apikey";
+
+    //Method to convert uri to url
+    //uri is information to be retrived and url is how we retrive it/directions to retrive it.
+    public static URL buildURLForWeather()
+    {
+        Uri uri = Uri.parse(BASE_URL).buildUpon()
+                .appendQueryParameter(API_PARAM, API_KEY)
+                .appendQueryParameter(METRIC_PARAM, METRIC_VALUE)
+                .build();
+
+        URL url =null;
+
+        try
+        {
+            url = new URL(uri.toString());
+        }
+        catch (MalformedURLException e)
+        {
+            throw new RuntimeException(e);
+        }
+
+        Log.i(TAG, "buildURLForWeather: "+url);
+
+        return url;
+    }
 }
